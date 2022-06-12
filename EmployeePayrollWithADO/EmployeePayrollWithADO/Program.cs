73e2b56c4b08fb956a9f1418fcd126f7e4892368
Viraj <jadhavviraj50@gmail.com>
2022-06-12 16:17:57 +0530

    [Viraj]-Added UC1-Create&ConnectDatabase file

---
+++ EmployeePayrollWithADO/EmployeePayrollWithADO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeePayrollWithADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll!");
+
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            repo.GetAllEmployee();
+
+        }
+    }
+}
