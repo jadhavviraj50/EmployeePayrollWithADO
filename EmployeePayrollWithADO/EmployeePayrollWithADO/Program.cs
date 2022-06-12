54ef488bc736027891e44f74c89918b381faab1a
Viraj <jadhavviraj50@gmail.com>
2022-06-12 16:41:30 +0530

    [Viraj]-Added UC2-RetrieveDatabase file

--- EmployeePayrollWithADO/EmployeePayrollWithADO/Program.cs
+++ EmployeePayrollWithADO/EmployeePayrollWithADO/Program.cs
@@ -10,7 +10,22 @@ namespace EmployeePayrollWithADO
 
             EmployeeRepo repo = new EmployeeRepo();
             EmployeeModel employee = new EmployeeModel();
-            repo.GetAllEmployee();
+            //repo.GetAllEmployee();
+
+            employee.name = "Bruce Wayne";
+            employee.salary = 25000;
+            employee.startdate = "05-05-2019";
+            employee.gender = 'M';
+            employee.phone = "0123586489";
+            employee.address = "3-161-21";
+            employee.department = "Hr";
+            employee.basicpay = 22000;
+            employee.deduction = 1500;
+            employee.taxablepay = 200;
+            employee.incometax = 5000;
+            employee.netpay = 25000;
+
+            repo.AddEmployee(employee);
 
         }
     }
